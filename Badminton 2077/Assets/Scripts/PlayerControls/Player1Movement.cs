562ea18220480f06241f2a02378f34049f35d6ad
LilGenius03 <C22486512@mytudublin.ie>
2023-09-25 13:33:13 +0100

    Some changes(Finn)
    Change some things and added some scripts

---
+++ Badminton 2077/Assets/Scripts/PlayerControls/Player1Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour
+{
+    public float distance = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+    }
+}
