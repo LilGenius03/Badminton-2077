55d2b32cf6602534696afaf2c22c0ae0d9de84b6
LilGenius03 <C22486512@mytudublin.ie>
2023-09-25 14:26:56 +0100

    Coding
    Added player1 movement doesn't work properly though

--- Badminton 2077/Assets/Scripts/PlayerControls/Player1Movement.cs
+++ Badminton 2077/Assets/Scripts/PlayerControls/Player1Movement.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class Player1Movement : MonoBehaviour
 {
-    public float distance = 1f;
+    public float speed = 0.2f;
+    private Rigidbody rb;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
+            transform.position = new Vector2(-3, 2);
 
         }
+        
     }
 }
