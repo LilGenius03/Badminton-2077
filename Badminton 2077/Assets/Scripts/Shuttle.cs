e45bc8bb4eb9da507b324a425d4f9a47770a8d35
Seatwentytwo <C22480762@mytudublin.ie>
2023-10-05 18:25:32 +0100

    Update Shuttle.cs

--- Badminton 2077/Assets/Scripts/Shuttle.cs
+++ Badminton 2077/Assets/Scripts/Shuttle.cs
@@ -22,11 +22,6 @@ public class Shuttle : MonoBehaviour
         ShuttleScored = false;
     }
 
-    private void Update()
-    {
-        Debug.Log(speed);
-    }
-
     IEnumerator Launch()
     {
 
