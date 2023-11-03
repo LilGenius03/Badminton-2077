6a507b70937c4496a7afb15f472d76dec2a1f923
FinnanB <123640726+FinnanB@users.noreply.github.com>
2023-11-03 18:55:40 +0000

    Revert "more fixes"
    This reverts commit 0b7c3b7a04f52aebcfdaec4d8578cd4c55da5b31.

--- Badminton 2077/Assets/Scripts/UI/SelectRacket.cs
+++ Badminton 2077/Assets/Scripts/UI/SelectRacket.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,16 +10,18 @@ using TMPro;
 
 public class SelectRacket : MonoBehaviour
 {
-    public string[] rNames = { "Power", "Rocket", "Zigzag" };
+    /*public bool Rocket1Chosen;
+    public bool Power1Chosen;
+    public bool ZigZag1Chosen;
+
+    public bool Rocket2Chosen;
+    public bool Power2Chosen;
+    public bool ZigZag2Chosen;*/
+
     public Sprite[] racket = new Sprite[3];
     public TextMeshProUGUI[] text = new TextMeshProUGUI[3];
     public GameObject shown;
-<<<<<<< HEAD
     public GameObject ShownText;
-=======
-    public TMP_Text rName;
-    public TMP_Text rNameShadow;
->>>>>>> 723c659191aa121d361cc1a3e08cc720dda3faca
     bool p1Ready;
     bool p2Ready;
 
@@ -60,14 +61,8 @@ public class SelectRacket : MonoBehaviour
             Options = 2;
         }
         shown.GetComponent<Image>().sprite = racket[Options];
-<<<<<<< HEAD
         ShownText.GetComponent<TextMeshProUGUI>().text = text[Options].text;
         if(Input.GetKeyDown(KeyCode.W))
-=======
-        rName.text = rNames[Options];
-        rNameShadow.text = rNames[Options];
-        if (Input.GetKeyDown(KeyCode.W))
->>>>>>> 723c659191aa121d361cc1a3e08cc720dda3faca
         {
             p1Ready = true;
         }
